1d3cdd40e5fb55d1f7e423be8b8ea75c77b23103
Varneon <me@varneon.com>
2023-05-23 07:38:50 -0700

    Add console window settings

--- Packages/com.varneon.vudon.logger/Editor/UdonConsoleEditor.cs
+++ Packages/com.varneon.vudon.logger/Editor/UdonConsoleEditor.cs
@@ -13,6 +13,16 @@ namespace Varneon.VUdon.Logger.Editor
         [SerializeField]
         private Texture2D bannerIcon;
 
+        private bool showWindowSettings;
+
+        private RectTransform canvasRectTransform;
+
+        private float windowWidth, windowHeight, windowScale;
+
+        private static readonly GUIContent WindowFoldoutContent = new GUIContent("Window", "Adjust console window");
+
+        private const string SHOW_WINDOW_SETTINGS_PERSISTENCE_KEY = "Varneon/VUdon/Logger/UdonConsole/Editor/Window";
+
         protected override string FoldoutPersistenceKey => "Varneon/VUdon/Logger/UdonConsole/Editor/Foldouts";
 
         protected override InspectorHeader Header => new InspectorHeaderBuilder()
@@ -21,5 +31,70 @@ namespace Varneon.VUdon.Logger.Editor
             .WithURL("GitHub", "https://github.com/Varneon/VUdon-Logger")
             .WithIcon(bannerIcon)
             .Build();
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            canvasRectTransform = ((UdonConsole)target).GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
+
+            Vector2 windowResolution = canvasRectTransform.sizeDelta;
+
+            windowWidth = windowResolution.x;
+
+            windowHeight = windowResolution.y;
+
+            windowScale = canvasRectTransform.localScale.x;
+
+            if (EditorPrefs.HasKey(SHOW_WINDOW_SETTINGS_PERSISTENCE_KEY))
+            {
+                showWindowSettings = EditorPrefs.GetBool(SHOW_WINDOW_SETTINGS_PERSISTENCE_KEY);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            EditorPrefs.SetBool(SHOW_WINDOW_SETTINGS_PERSISTENCE_KEY, showWindowSettings);
+        }
+
+        protected override void OnPreDrawFields()
+        {
+            if (showWindowSettings = EditorGUILayout.BeginFoldoutHeaderGroup(showWindowSettings, WindowFoldoutContent))
+            {
+                GUI.color = Color.black;
+
+                using (EditorGUILayout.VerticalScope verticalScope = new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+                {
+                    GUI.Box(verticalScope.rect, string.Empty);
+
+                    GUI.color = Color.white;
+
+                    using (new EditorGUI.IndentLevelScope(1))
+                    {
+                        using (EditorGUI.ChangeCheckScope changeScope = new EditorGUI.ChangeCheckScope())
+                        {
+                            windowWidth = EditorGUILayout.Slider("Width", windowWidth, 1000f, 2000f);
+
+                            windowHeight = EditorGUILayout.Slider("Height", windowHeight, 500f, 2000f);
+
+                            windowScale = Mathf.Clamp(EditorGUILayout.FloatField("Scale", windowScale), 0.0001f, 10f);
+
+                            if (changeScope.changed)
+                            {
+                                Undo.RecordObject(canvasRectTransform, "Adjust UdonConsole Window");
+
+                                canvasRectTransform.sizeDelta = new Vector2(windowWidth, windowHeight);
+
+                                canvasRectTransform.localScale = Vector3.one * windowScale;
+                            }
+                        }
+                    }
+                }
+            }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
     }
 }
